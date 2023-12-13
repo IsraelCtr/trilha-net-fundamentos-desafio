6155d72b6ca2c909483b483361cbbfcdfacb930d
Israel Castro <94873358+IsraelCtr@users.noreply.github.com>
2023-12-13 00:08:00 -0300

    retirando modificação de teste

--- DesafioFundamentos/Models/Estacionamento.cs
+++ DesafioFundamentos/Models/Estacionamento.cs
@@ -36,7 +36,7 @@ namespace DesafioFundamentos.Models
                 // TODO: Realizar o seguinte cálculo: "precoInicial + precoPorHora * horas" para a variável valorTotal                
                 // *IMPLEMENTE AQUI*
                 int horas = 0;
-                int horastotal = 1;
+
                 decimal valorTotal = 0; 
 
                 // TODO: Remover a placa digitada da lista de veículos
